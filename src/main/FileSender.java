13321c0e85e794db605d8f9f833a4ebe82d79075
VeloZorn <VeloZorn@users.noreply.github.com>
2019-01-29 15:42:39 +0100

    Von Massenimport auf Zip Export umgestiegen Support für Outlook und Thunderbird Windows 10 Mail ausstehend

--- src/main/FileSender.java
+++ src/main/FileSender.java
@@ -8,17 +8,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.IOUtils;
 
 public class FileSender {
 
-	// TODO Mehrere Files auf einmal versenden und einlesen || Thunderbird Supported --- Outlook + Windows 10 Mail nicht
-	// TODO GUI mit Namensgebung
-
-	private static List<String> fileInputPath = new ArrayList<String>(); // = "C:\\Users\\amkor\\Desktop\\test.txt";
-	private static List<String> fileOutputPath = new ArrayList<String>(); // = "C:\\Users\\amkor\\Desktop\\demo.txt";
-	private static boolean send;
+	//TODO Datei muss nach Senden gelöscht werden 
+	
+	private static List<String> fileInputPath = new ArrayList<String>();
+	private static List<String> fileOutputPath = new ArrayList<String>();
+	private static String zipName = "Zip";
+	private static String zipOut = "C:\\Users\\amkor\\Desktop\\Test Files\\Output\\" + zipName + ".zip";
 
 	public static void main(String[] args) {
 
@@ -34,9 +36,12 @@ public class FileSender {
 		fileOutputPath.add("C:\\Users\\amkor\\Desktop\\Test Files\\Output\\demo5.txt");
 
 		try {
-			// File in .tmp umwandeln und in Anhang anfügen
-			createTemporaryFile();
+			//createTemporaryFile();
+			zipOutput();
 			composeEmail();
+			File deleteFile = new File(zipOut);
+			Thread.sleep(10000);
+			deleteFile.delete();
 		} catch (Exception err) {
 			err.printStackTrace();
 		}
@@ -45,36 +50,21 @@ public class FileSender {
 
 	public static void composeEmail() throws Exception {
 
-		boolean thunderbird = false;
-		boolean outlook = true;
+		boolean thunderbird = true;
+		boolean outlook = false;
 		String completeAttachment = "";
-		send = false;
 
 		// Run-Befehl je nach verwendetem Mail Programm erzeugen
 		String cmd = ""; // TODO Über Input Stream
 		String os = System.getProperty("os.name").toLowerCase();
 		// Thunderbird
 		if (os.contains("win") && thunderbird == true && outlook == false) {
-			for (int i = 0; i < fileInputPath.size(); i++) {
-				if (i < (fileInputPath.size() - 1)) {
-					completeAttachment = completeAttachment + "file:///" + fileOutputPath.get(i) + ",";
-				} else {
-					completeAttachment = completeAttachment + "file:///" + fileOutputPath.get(i);
-					cmd = "cmd /c start thunderbird.exe -compose attachment='" + completeAttachment + "'";
-					System.out.println(completeAttachment);
-				}
-			}
+			cmd = "cmd /c start thunderbird.exe -compose attachment='" + "file:///" + zipOut + "'";
 		}
-		//TODO Outlook || Outlook cmd line switch akzeptiert nicht mehrere Attachments || Mögliche Lösung Zip Archiv
 		else if (os.contains("win") && thunderbird == false && outlook == true) {
-			String dokumente = "";
-			for(String path : fileOutputPath) {
-				dokumente += path + ";";
-			}
-			cmd = "cmd /c start outlook.exe /a " + dokumente;
-		}
-		else if(os.contains("win") && thunderbird == false && outlook == false){
-			
+			cmd = "cmd /c start outlook.exe /a " + '"' + zipOut + '"';
+		} else if (os.contains("win") && thunderbird == false && outlook == false) {
+
 		}
 
 		// Mail-Client mit Parametern starten
@@ -82,23 +72,45 @@ public class FileSender {
 
 	}
 
-	public static void createTemporaryFile() throws IOException {
-		for (int i = 0; i < fileInputPath.size(); i++) {
-			try {
-				InputStream fileInput = new FileInputStream(fileInputPath.get(i));
-				FileOutputStream tmpFile = new FileOutputStream(new File(fileOutputPath.get(i)));
-				int byteRead;
-				while ((byteRead = fileInput.read()) != -1) {
-					tmpFile.write(byteRead);
-				}
+	/*
+	 * public static void createTemporaryFile() throws IOException { for (int i = 0;
+	 * i < fileInputPath.size(); i++) { try { InputStream fileInput = new
+	 * FileInputStream(fileInputPath.get(i)); FileOutputStream tmpFile = new
+	 * FileOutputStream(new File(fileOutputPath.get(i))); int byteRead; while
+	 * ((byteRead = fileInput.read()) != -1) { tmpFile.write(byteRead); }
+	 * 
+	 * } catch (FileNotFoundException e) { System.out.println("Could not convert " +
+	 * fileInputPath); e.printStackTrace(); break; } } }
+	 */
 
-			} catch (FileNotFoundException e) {
-				System.out.println("Could not convert " + fileInputPath);
-				e.printStackTrace();
-				send = false;
-				break;
+	public static void zipOutput() {
+
+		try {
+
+			byte[] buffer = new byte[1024];
+
+			FileOutputStream outFile = new FileOutputStream(zipOut);
+			ZipOutputStream outZip = new ZipOutputStream(outFile);
+
+			for (int i = 0; i < fileInputPath.size(); i++) {
+
+				File srcFile = new File(fileInputPath.get(i));
+				FileInputStream inFile = new FileInputStream(srcFile);
+				
+				outZip.putNextEntry(new ZipEntry(srcFile.getName()));
+				
+				int j;
+				while((j = inFile.read(buffer)) > 0) {
+					outZip.write(buffer, 0, j);
+				}
+				outZip.closeEntry();
+				inFile.close();
 			}
-			send = true;
+			outZip.close();
+			
+			
+		} catch (IOException e) {
+			System.out.println("Fehler");
 		}
 	}
 }
